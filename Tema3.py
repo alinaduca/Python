812ade24ad0fade305fd5591925d2472688e8f8d
Alina Duca <alina_duca@yahoo.com>
2023-10-24 15:56:12 +0300

    Update Tema3

--- Tema3.py
+++ Tema3.py
@@ -126,7 +126,7 @@ def ex5():
 
 # 6. Write a function that receives as a parameter a variable number of lists and a whole number x. Return a list containing the
 # items that appear exactly x times in the incoming lists. 
-# Example: For the [1,2,3], [2,3,4],[4,5,6], [4,1, "test"] and x = 2 lists [1,2,3 ] # 1 is in list 1 and 4, 2 is in list 1
+# Example: For the [1,2,3], [2,3,4], [4,5,6], [4,1, "test"] and x = 2 lists [1,2,3 ] # 1 is in list 1 and 4, 2 is in list 1
 # and 2, 3 is in lists 1 and 2.
 
 #Ex 6
@@ -282,6 +282,6 @@ def group_of_rhyme(L):
     return list_of_lists
 
 def ex12():
-    L = ["ana", "banana","carte","arme","parte"]
+    L = ["ana", "banana", "carte", "arme", "parte"]
     print(group_of_rhyme(L))
 # ex12()
